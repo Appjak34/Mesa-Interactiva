93b37412be25133463469498ba90687ec0b4d287
Angel Emilio Lopez Fernandez <appjak34@iMac-de-Angel.local>
2019-02-09 22:45:26 -0600

    Creacion de html, junto con la implementacion del drag and drop en js, con estilos pequeños en css -INICIO DEL PROYECTO

---
+++ src/js/scripts.js
@@ -0,0 +1,83 @@
+function drawImg() {
+    var x = document.getElementById('canvasAreaOne');
+    var canvax = x.getContext('2d');	//getContext untuk mendeklarasikan dimensi canvas yang kita buat di var x
+    var imgElement = document.getElementById('imgPhoneOne');
+    var imgObj = new Image();
+    imgObj.src = imgElement.src;
+
+    var imgW = imgObj.width;
+    var imgH = imgObj.height;
+    var imgX = (canvax.canvas.width * .5) - (imgW * .5);
+    var imgY = (canvax.canvas.height * .5) - (imgH * .5);
+
+    //ev.setData("imgWidth", canvax.imgW);
+
+    imgObj.onload = function () {			//load image on canvas
+        canvax.clearRect(imgX, imgY, imgW, imgH);			//bersihkan canvas dari gambar sebelumnya
+        canvax.drawImage(imgObj, imgX, imgY, imgW, imgH);	//place image on canvas in x & y coordinat = 10
+    };
+
+}
+
+function drawImg2() {
+    var y = document.getElementById('canvasAreaTwo');
+    var canvay = y.getContext('2d');	//getContext untuk mendeklarasikan dimensi canvas yang kita buat di var x
+    var imgElementY = document.getElementById('imgPhoneTwo');
+    var imgObj2 = new Image();
+    imgObj2.src = imgElementY.src;
+
+    var imgW = imgObj2.width;
+    var imgH = imgObj2.height;
+    var imgX = (canvay.canvas.width * .5) - (imgW * .5);
+    var imgY = (canvay.canvas.height * .5) - (imgH * .5);
+
+
+    imgObj2.onload = function () {			//load image on canvas
+        canvay.clearRect(imgX, imgY, imgW, imgH);			//bersihkan canvas dari gambar sebelumnya
+        canvay.drawImage(imgObj2, imgX, imgY, imgW, imgH);	//place image on canvas in x & y coordinat = 10
+    };
+
+}
+
+$(document).ready(function () {
+
+    $('.listImg li').draggable({
+            containment: 'document',
+            opacity: 0.60,
+            revert: false,
+            helper: 'clone'
+        }
+    );
+
+    $('#canvasAreaOne').droppable({
+        hoverClass: 'borda', tolerance: 'pointer',
+        drop: function (ev, ui) {
+            var droppedItem = $(ui.draggable).clone();
+            var canvasImg = $(this).find('img');
+            var newSrc = droppedItem.find('img').attr('src');
+            canvasImg.attr("src", newSrc);
+            drawImg();
+        }
+    });
+
+    $('#canvasAreaOne').dblclick(function () {
+        $('#canvasAreaOne').draggable();
+
+    });
+    $('#canvasAreaTwo').droppable({
+        hoverClass: 'borda', tolerance: 'pointer',
+        drop: function (ev, ui) {
+            var droppedItem = $(ui.draggable).clone();
+            var canvasImg = $(this).find('img');
+            var newSrc = droppedItem.find('img').attr('src');
+            canvasImg.attr("src", newSrc);
+            drawImg2();
+        }
+    });
+
+    $('#canvasAreaTwo').dblclick(function () {
+        $('#canvasAreaTwo').draggable();
+
+    });
+
+});
