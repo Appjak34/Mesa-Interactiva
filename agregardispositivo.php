8d08103d2fde86ab46d760b154eab3f6e8445a71
Josafat Romero Flores <yossask8@hotmail.com>
2019-02-18 23:29:21 -0600

    Create agregardispositivo.php

---
+++ agregardispositivo.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+	include "./database/conexion.php";
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8"/>
+	<title>Panel de Administración</title>
+</head>
+<body>
+
+	<center><h1>Agregar un Nuevo Dispositivo</h1></center>
+	<form action="altadispositivo.php" method = "post" enctype="multipart/form-data">
+		<fieldset>
+			Imagen<br>
+			<input type="file" name="file">
+		</fieldset>
+				<fieldset>
+			Nombre<br>
+			<input type="text" name="nombre">
+		</fieldset>
+		<fieldset>
+			precio<br>
+			<input type="text" name="precio">
+		</fieldset>
+		<fieldset>
+			Marca<br>
+			<input type="text" name="marca">
+		</fieldset>
+		<fieldset>
+			Modelo<br>
+			<input type="text" name="modelo">
+		</fieldset>
+		<fieldset>
+			Descripción<br>
+			<input type="text" name="descripcion">
+		</fieldset>
+		<fieldset>
+			Color<br>
+			<input type="text" name="color">
+		</fieldset>
+		<input type="submit" name="accion" value="Enviar" class="aceptar">
+	</form>	
+	
+	
+</body>
+</html>
